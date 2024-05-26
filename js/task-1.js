eee42631f02be39bf27dbb233e627c4e1200db41
Yaroslav Chmyr <yaroslav.ch72@gmail.com>
2024-05-26 14:47:11 +0300

    Task 2 done.

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,20 @@
 'use strict';
 
 function isEnoughCapacity(products, containerSize) {
-  let totalProducts = 0
+  let totalProducts = 0;
   for (const product of Object.values(products)) {
     totalProducts += product;
   }
   if (totalProducts > containerSize) {
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
 
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-);
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
 
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-);
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
 
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-);
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-);
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
